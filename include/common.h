8efaff3d92bf82564b2e510202ba209761c597f3
GODGIRII <rishabgirii28@gmail.com>
2026-04-19 00:35:47 +0530

    Created include folder with common.h file

---
+++ include/common.h
@@ -0,0 +1,22 @@
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define PORT    "3000"
+#define BACKLOG 10
+#define BUFSIZE 4096
+
+int send_all(int fd, const char *buf, int len);
+
+#endif
